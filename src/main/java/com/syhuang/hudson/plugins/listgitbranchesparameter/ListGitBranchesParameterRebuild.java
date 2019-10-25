85bdec6810544c38ced466964927ff4d9e63b8e2
Sean Ly <seanly@aliyun.com>
2019-10-25 13:05:46 +0800

    fix tagFilter defaultValue

---
+++ src/main/java/com/syhuang/hudson/plugins/listgitbranchesparameter/ListGitBranchesParameterRebuild.java
@@ -0,0 +1,19 @@
+package com.syhuang.hudson.plugins.listgitbranchesparameter;
+
+import com.sonyericsson.rebuild.RebuildParameterPage;
+import com.sonyericsson.rebuild.RebuildParameterProvider;
+import hudson.Extension;
+import hudson.model.ParameterValue;
+
+@Extension
+public class ListGitBranchesParameterRebuild extends RebuildParameterProvider {
+    @Override
+    public RebuildParameterPage getRebuildPage(ParameterValue parameterValue) {
+
+        if (parameterValue instanceof ListGitBranchesParameterValue) {
+            return new RebuildParameterPage(parameterValue.getClass(),"value.jelly");
+        } else {
+            return null;
+        }
+    }
+}
